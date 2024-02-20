4da0381ee9e71c131c09288bbf24977118201124
Giovani de Oliveira <giovani98@proton.me>
2024-02-19 21:37:53 -0300

    feat: Creates UI

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,80 @@
-import { Plus } from 'lucide-react'
+import { FileDown, MoreHorizontal, Plus, Search } from 'lucide-react'
+import { Header } from './components/header'
+import { Tabs } from './components/tabs'
+import { Button } from './components/ui/button'
+import { Control, Input } from './components/ui/input'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './components/ui/table'
+import { Pagination } from './components/pagination'
 
 export function App() {
   return (
     <div className="py-10 space-y-8">
-      <div>header & abas</div>
+      <div>
+        <Header />
+        <Tabs />
+      </div>
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <button className="inline-flex items-center gao-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1">
-            <Plus className="size-3" /> Create New
-          </button>
+          <Button variant="primary">
+            <Plus className="size-3" /> Create new
+          </Button>
         </div>
+
+        <div className="flex items-center justify-between">
+          <Input variant="filter">
+            <Search className="size-3" />
+            <Control placeholder="Search tags..." />
+          </Input>
+
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
+        </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 10 }).map((_value, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col gap-0.5">
+                      <span className="font-medium">React</span>
+                      <span className="font-xs text-zinc-500">
+                        fba94345-fefd-4ec8-aa53-84b689a05d74
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-300">13 videos</TableCell>
+                  <TableCell className="text-right">
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+
+        {/* <Pagination /> */}
       </main>
     </div>
   )
